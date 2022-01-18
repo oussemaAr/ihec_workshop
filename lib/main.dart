54b458610671b1badb2fc33d1fb907d53debf92b
oussemaAr <ar.oussema@gmail.com>
2022-01-18 11:48:07 +0100

    Adding InformationText widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:ihec_workshop/screen/mycard.screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(),
       home: MyCard(),
     ),
   );
--- lib/screen/mycard.screen.dart
+++ lib/screen/mycard.screen.dart
@@ -1,20 +1,37 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class MyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: Colors.teal,
-        child: Column(
-          children: [
-            const CircleAvatar(
-              backgroundImage: AssetImage('images/image.jpg'),
-              radius: 60,
-            ),
-          ],
+    return Material(
+      child: SafeArea(
+        child: Container(
+          color: Colors.teal,
+          child: Column(
+            children: [
+              const CircleAvatar(
+                backgroundImage: AssetImage('images/image.jpg'),
+                radius: 60,
+              ),
+              informationText('Oussema AROUA', 36),
+              informationText('Mobile Developer', 28)
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  informationText(String contentText, double contentFontSize) {
+    return Text(
+      contentText,
+      style: TextStyle(
+          fontFamily: 'AdventPro',
+          fontSize: contentFontSize,
+          color: Colors.white,
+          letterSpacing: 2),
     );
   }
 }

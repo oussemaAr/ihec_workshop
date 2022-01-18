c8e21745e76f9f17305b0e134cd4a010e0b552e4
oussemaAr <ar.oussema@gmail.com>
2022-01-18 10:48:25 +0100

    Adding SafeArea

--- lib/screen/mycard.screen.dart
+++ lib/screen/mycard.screen.dart
@@ -3,15 +3,17 @@ import 'package:flutter/material.dart';
 class MyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal,
-      child: Column(
-        children: [
-          const CircleAvatar(
-            backgroundImage: AssetImage('images/image.jpg'),
-            radius: 60,
-          ),
-        ],
+    return SafeArea(
+      child: Container(
+        color: Colors.teal,
+        child: Column(
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage('images/image.jpg'),
+              radius: 60,
+            ),
+          ],
+        ),
       ),
     );
   }

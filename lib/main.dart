c90ae4ffe0eed5ba65d88ca136a30c3244660022
oussemaAr <ar.oussema@gmail.com>
2022-01-18 10:35:38 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ihec_workshop/screen/mycard.screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyCard(),
+    ),
+  );
+}
